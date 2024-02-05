f381ff2acea4c5ebb485fa6f89f8166bc960e32f
TristanM <tristanmoffett36@gmail.com>
2024-02-04 20:18:07 -0700

    Commit for all files related to local_library catalog appliation

---
+++ StartUp.py
@@ -0,0 +1,14 @@
+# Installing Django through a VE
+#   python -m venv <namespace>
+#   Enter <namespace>
+#   py -3 -m pip install django~=4.2
+#   py -3 -m django --version
+
+# Test if django is installed using:
+#   mkdir django_test
+#   cd django_test
+#   django-admin startproject mytestsite (Creates a Django website)
+#   cd mytestsite       
+#   py -3 manage.py runserver           (Runs it in browser)
+
+print("Console Start")
---
+++ django_projects/local_library/catalog/urls.py
@@ -0,0 +1,7 @@
+# This is where all patterns will go as the application is built
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
